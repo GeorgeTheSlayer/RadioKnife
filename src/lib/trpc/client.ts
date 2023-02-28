624df020271b583aab49d5afbc965992670d5cb6
GeorgeTheSlayer <holland@sersen.net>
2023-02-28 16:40:56 -0600

    trpc try 9

--- src/lib/trpc/client.ts
+++ src/lib/trpc/client.ts
@@ -11,9 +11,21 @@ export const trpc = createTRPCProxyClient<AppRouter>({
       // url is dependent on the environment
       // in dev, we use localhost
       // in prod, we use the public domain
-   url: dev
-    ? 'http://localhost:5173/api/trpc'
-    : 'https://' + prodEndpoint + '/api/trpc',
+   url: 'http://localhost:5173/api/trpc',
   }),
  ],
 });
+
+
+// export const trpc = createTRPCProxyClient<AppRouter>({
+//   links: [
+//    httpBatchLink({
+//        // url is dependent on the environment
+//        // in dev, we use localhost
+//        // in prod, we use the public domain
+//     url: dev
+//      ? 'http://localhost:5173/api/trpc'
+//      : 'https://' + prodEndpoint + '/api/trpc',
+//    }),
+//   ],
+//  });
