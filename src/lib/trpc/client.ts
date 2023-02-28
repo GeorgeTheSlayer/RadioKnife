0768d6078937871a38ec52e9b67427284cc92ac8
GeorgeTheSlayer <holland@sersen.net>
2023-02-28 15:30:58 -0600

    trpc try 4

--- src/lib/trpc/client.ts
+++ src/lib/trpc/client.ts
@@ -1,11 +1,9 @@
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import type { AppRouter } from './server';
 import { dev } from '$app/environment';
-import dotenv from 'dotenv';
 
-dotenv.config();
-const prodEndpoint = process.env.TRPC_ENDPOINT!;
-const apiUrl = process.env.VERCEL_URL!;
+const prodEndpoint = import.meta.env.VERCEL_URL;
+//const apiUrl = process.env.VERCEL_URL!;
 
 export const trpc = createTRPCProxyClient<AppRouter>({
  links: [
@@ -15,7 +13,7 @@ export const trpc = createTRPCProxyClient<AppRouter>({
       // in prod, we use the public domain
    url: dev
     ? 'http://localhost:5173/api/trpc'
-    : apiUrl + '/api/trpc',
+    : prodEndpoint + '/api/trpc',
   }),
  ],
 });
--- src/routes/(app)/synth/view/+page.svelte
+++ src/routes/(app)/synth/view/+page.svelte
@@ -2,7 +2,6 @@
 	import MaxSynth from '$lib/components/MaxSynth.svelte';
 	import { browser } from '$app/environment';
 	import { page } from '$app/stores';
-	import { trpc } from '$lib/trpc/client';
 
 	//Get Seach Params and show on browser
 	const URL = $page.url;
