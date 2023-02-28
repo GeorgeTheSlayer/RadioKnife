b64b24f76187c4d537c36c3ff7c7aecfb7912c4a
GeorgeTheSlayer <holland@sersen.net>
2023-02-28 11:54:34 -0600

    added env

--- src/lib/trpc/client.ts
+++ src/lib/trpc/client.ts
@@ -1,6 +1,11 @@
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import type { AppRouter } from './server';
 import { dev } from '$app/environment';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const prodEndpoint = process.env.TRPC_ENDPOINT!;
+const apiUrl = process.env.VERCEL_URL!;
 
 export const trpc = createTRPCProxyClient<AppRouter>({
  links: [
@@ -10,7 +15,7 @@ export const trpc = createTRPCProxyClient<AppRouter>({
       // in prod, we use the public domain
    url: dev
     ? 'http://localhost:5173/api/trpc'
-    : `https://example.com/api/trpc`,
+    : apiUrl,
   }),
  ],
 });
