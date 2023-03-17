bd5378908a5750edb44bbdb0d4ea7f450832733f
GeorgeTheSlayer <holland@sersen.net>
2023-03-17 14:03:33 -0500

    Added simple error Messages

--- src/routes/(app)/forum/+page.server.ts
+++ src/routes/(app)/forum/+page.server.ts
@@ -9,7 +9,7 @@ export const load: PageServerLoad = async () => {
 	};
 
 	try {
-		return { forums: forums() } as const;
+		return { forums: await forums() } as const;
 	} catch (err) {
 		console.log(err);
 		throw error(500, 'Check the console');
