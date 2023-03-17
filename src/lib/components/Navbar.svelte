5a65f6fe12d21690dde87946311fe3747ab3b28d
GeorgeTheSlayer <holland@sersen.net>
2023-03-17 13:06:39 -0500

    Made header responsive

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	// import Hamburger from './Hamburger.svelte';
-	// import Search from './Search.svelte';
 	import { slide } from 'svelte/transition';
 	import { beforeNavigate } from '$app/navigation';
 	import { getUser } from '@lucia-auth/sveltekit/client';
@@ -13,6 +11,10 @@
 
 	function flipSearch() {
 		isSearch = !isSearch;
+	}
+
+	function flipper() {
+		isFlipped = !isFlipped;
 	}
 
 	beforeNavigate(() => {
@@ -38,32 +40,58 @@
 		rel="stylesheet"
 		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
 	/>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+	/>
 </svelte:head>
 
-<div class=" grid h-20 w-full grid-cols-3 border-b-2 border-b-black">
-	<div class="mx-auto" />
-	<ul class=" flex h-full w-fit flex-nowrap items-center gap-x-36 text-center">
-		<li class="h-fit w-fit rounded hover:bg-pastel-p "><a href="/docs"><h2>Learn</h2></a></li>
+<div class="block w-full border-b-2 border-b-black md:flex">
+	<div class="max-w-1/3 hidden md:block md:w-1/3" />
+	<ul class="flex h-full w-full flex-nowrap items-center justify-evenly text-center">
+		<li class=" rounded hover:bg-pastel-p ">
+			<button class="block md:hidden" on:click={flipSearch}>
+				{#if isSearch}
+					<span class="material-symbols-outlined md-48"> close </span>
+				{:else}
+					<span class="material-symbols-outlined md-48"> search </span>
+				{/if}
+			</button>
+			<a class="hidden md:block" href="/docs"><h2>Learn</h2></a>
+		</li>
 		<li class="h-fit w-fit rounded hover:bg-pastel-p ">
 			<a href="/home"><h1 class="text-5xl font-bold">RadioKnife</h1></a>
 		</li>
-		<li class="h-fit w-fit rounded hover:bg-pastel-p ">
-			<a href="/synth/create"><h2>Create</h2></a>
+		<li class="rounded hover:bg-pastel-p">
+			<button class="block md:hidden" on:click={flipper}>
+				{#if isFlipped}
+					<span class="material-symbols-outlined md-48"> close </span>
+				{:else}
+					<span class="material-symbols-outlined md-48"> menu </span>
+				{/if}
+			</button>
+			<a class="hidden md:block" href="/synth/create"><h2>Create</h2></a>
 		</li>
-		<!--	<li class=" h-3 w-3">m</li>-->
-		<!--	<li>a</li>-->
-		<!--	<li>a</li>-->
 	</ul>
-	<ul class="basis-1/7 ml-auto mr-10 flex w-fit grow-0 items-center gap-x-2">
-		<li class=" rounded hover:bg-pastel-p">
+	<ul class=" ml-auto hidden w-fit grow-0 items-center gap-x-2 md:flex">
+		<li class=" flex rounded hover:bg-pastel-p">
 			{#if isSearch}
-				<div transition:slide|local class="w-full border-4 border-b-black text-primary-b">
+				<div transition:slide|local class="flex h-full w-full border-4 border-black text-primary-b">
 					<input type="search" name="searchBar" id="searchBar" />
+					<span class="material-symbols-outlined md-48"> search </span>
 				</div>
 			{/if}
-			<button on:clic={flipSearch}
-				><span class="material-symbols-outlined md-48"> search </span></button
-			>
+			<button on:click={flipSearch}>
+				{#if isSearch}
+					<span class="material-symbols-outlined md-48"> close </span>
+				{:else}
+					<span class="material-symbols-outlined md-48"> search </span>
+				{/if}
+			</button>
 		</li>
 		<li class=" rounded hover:bg-pastel-p">
 			<a href="/forum" class=""><span class="material-symbols-outlined md-48"> forum </span></a>
@@ -78,69 +106,25 @@
 			{/if}
 		</li>
 	</ul>
+	{#if isSearch}
+		<div transition:slide|local class="flex w-full border-2 border-black text-primary-b md:hidden">
+			<input class="w-full" type="search" name="searchBar" id="searchBarMobile" />
+			<span class="material-symbols-outlined md-48"> search </span>
+		</div>
+	{/if}
+	{#if isFlipped}
+		<div class=" w-full border-t-2 border-t-pastel-b md:hidden">
+			<ul transition:slide|local class="align-center flex flex-col text-left">
+				{#if $user}
+					<li class="hover:bg-pastel-p">
+						<a href={'/account?id=' + userId}><h1>Account</h1></a>
+					</li>
+				{:else}
+					<li class="hover:bg-pastel-p "><a href="/login"><h1>Login</h1></a></li>
+				{/if}
+				<li class="hover:bg-pastel-p"><a href="/forum"><h1>Forum</h1></a></li>
+				<li class="hover:bg-pastel-p"><a class="" href="/docs"><h1>Learn</h1></a></li>
+			</ul>
+		</div>
+	{/if}
 </div>
-
-<!--{#if isFlipped}-->
-<!--	<ul transition:slide|local class="m-0 p-0 text-left flex flex-col justify-between  ">-->
-<!--		{#if $user}-->
-<!--			<li-->
-<!--				class=" w-full p-2 bg-primary-b hover:bg-primary-g hover:text-white  transition duration-500 ease-in-out"-->
-<!--			>-->
-<!--				<a href={'/account?id=' + userId} class="block text-center">{username}</a>-->
-<!--			</li>-->
-<!--		{:else}-->
-<!--			<li-->
-<!--				class=" w-full p-2 bg-mono-30 hover:bg-mono-10 hover:text-white  transition duration-500 ease-in-out"-->
-<!--			>-->
-<!--				<a href="/login" class="block text-center">Login</a>-->
-<!--			</li>-->
-<!--		{/if}-->
-<!--		<li-->
-<!--			class=" w-full p-2 bg-mono-30 hover:bg-mono-10 hover:text-white  transition duration-500 ease-in-out"-->
-<!--		>-->
-<!--			<a href="/room" class="block text-center">Rooms</a>-->
-<!--		</li>-->
-<!--		<li-->
-<!--			class=" w-full p-2 bg-mono-30 hover:bg-mono-10 hover:text-white  transition duration-500 ease-in-out"-->
-<!--		>-->
-<!--			<a href="/synth/create" class="block text-center">Create</a>-->
-<!--		</li>-->
-<!--		<li-->
-<!--			class=" w-full p-2 bg-mono-30 hover:bg-mono-10 hover:text-white  transition duration-500 ease-in-out"-->
-<!--		>-->
-<!--			<a href="/forum" class="block text-center">Forum</a>-->
-<!--		</li>-->
-<!--	</ul>-->
-
-<!--{/if}-->
-<style>
-	/* Rules for sizing the icon. */
-	/*.material-symbols-outlined.md-18 {*/
-	/*	font-size: 18px;*/
-	/*}*/
-	/*.material-symbols-outlined.md-24 {*/
-	/*	font-size: 24px;*/
-	/*}*/
-	/*.material-symbols-outlined.md-36 {*/
-	/*	font-size: 36px;*/
-	/*}*/
-	.material-symbols-outlined.md-48 {
-		font-size: 48px;
-	}
-
-	/* Rules for using icons as black on a light background. */
-	/*.material-symbols-outlined.md-dark {*/
-	/*	color: rgba(0, 0, 0, 0.54);*/
-	/*}*/
-	/*.material-symbols-outlined.md-dark.md-inactive {*/
-	/*	color: rgba(0, 0, 0, 0.26);*/
-	/*}*/
-
-	/*!* Rules for using icons as white on a dark background. *!*/
-	/*.material-icons.md-light {*/
-	/*	color: rgba(255, 255, 255, 1);*/
-	/*}*/
-	/*.material-icons.md-light.md-inactive {*/
-	/*	color: rgba(255, 255, 255, 0.3);*/
-	/*}*/
-</style>
