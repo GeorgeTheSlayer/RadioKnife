f5381763f030ee5898d0df28cc5a519b0b3d7961
GeorgeTheSlayer <holland@sersen.net>
2023-03-17 13:58:32 -0500

    Added simple error Messages

--- src/routes/(app)/forum/+page.server.ts
+++ src/routes/(app)/forum/+page.server.ts
@@ -1,10 +1,17 @@
 import { prisma } from '$lib/server/prisma';
 import type { PageServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
+// import { redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async () => {
 	const forums = async () => {
 		return await prisma.thread.findMany();
 	};
 
-	return { forums: forums() } as const;
+	try {
+		return { forums: forums() } as const;
+	} catch (err) {
+		console.log(err);
+		throw error(500, 'Check the console');
+	}
 };
